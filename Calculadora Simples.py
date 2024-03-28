55559777839e476817cc7116c2eace0111f7fe7f
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-03-27 21:10:53 -0300

    1.3

--- Calculadora Simples.py
+++ Calculadora Simples.py
@@ -1,27 +1,29 @@
 resul = 0
-sair = True
-esc = 0
+sair = 1
 
-while  sair == True:
+while  sair == 1:
+ calc = (input("Escolha a opção:\n 1)Soma\n 2)Subtração\n 3)Multiplicação\n 4)Divisão\n 0)Sair\n"))
+ if calc == "0":
+    print("Encerrando Calculo")
+    sair = 0
+    continue
  num1 = float(input("Digite um numero:"))
- calc = (input("Escolha a opção: +, -, *  ou /:\n"))
  num2 = float(input("Digite  outro numero: "))
- if calc == "+":
+ if calc == "1":
     resul = num1 + num2
- elif calc == "-":
+    calc =  "+"
+ elif calc == "2":
     resul = num1 - num2
- elif calc == "*": 
+    calc = "-"
+ elif calc == "3": 
     resul = num1 * num2
- elif calc == "/":
+    calc = "*"
+ elif calc == "4":
     resul ==  num1 / num2
- else :
-    print("Número ou opção incorretos, Digite novamente")
-
- esc = int(input("Para finalizar a conta digite 5"))
- if esc == 5:
-   sair = False
-
-print (80*"-")
-print (f"A operação foi soma: {num1} {calc} {num2} = {resul}")
+    calc = "/"
+ else:
+     print ("Opção Inválida, Digite Novamente")
+ print (80*"-")
+ print (f"A operação foi: {num1} {calc} {num2} = {resul}")
 
     
