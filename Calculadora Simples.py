f1cf39cd7c503493906e7a48ac807a35375affe4
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-05-06 21:32:16 -0300

    Calculadora com função
    Estabelecido funções para cada conta e para verificar digitos invalidos

--- Calculadora Simples.py
+++ Calculadora Simples.py
@@ -1,48 +1,82 @@
 resul = 0
 sair = 1
 
+#-----Funções-----#
+
+def calc_soma(num1,num2):
+    return num1+num2
+
+def calc_sub(num1,num2):
+    return num1-num2
+
+def calc_div(num1,num2):
+    return num1/num2
+
+def calc_multi(num1,num2):
+    return num1*num2
+
+def somente_opcao(prompt):  #Para verificar se o usuario digitou uma letra ou um numero fora da lista que possa causar um erro de valor retornando "none"
+    while True:
+        try:
+            calc = int((input(prompt)))
+            if calc in [0, 1, 2, 3, 4]:
+              return calc
+            else:
+                print("Opção invalida, tente novamente.")
+        except ValueError:
+            print("Digito invalido, tente por numeros")
+
+def somente_num(prompt):    #Mesma coisa que o anterior mas somente para letras
+    while True:
+        try:
+            num = float(input(prompt))
+            return num
+        except ValueError:
+            print("Digito invalido, tente por numeros")
+
+#-----Fim_Funções-----#
+
+
+#-----Inicio_Menu-----#
+
+sair = 1
 while  sair == 1:
-    calc = int(input("Escolha a opção:\n 1)Soma\n 2)Subtração\n 3)Multiplicação\n 4)Divisão\n 0)Sair\n"))
-    if calc == "0":
+    calc = somente_opcao("Escolha a opção:\n 1)Soma\n 2)Subtração\n 3)Multiplicação\n 4)Divisão\n 0)Sair\n")
+    if calc == 0:
         print("Encerrando Calculo")
         sair = 0
-        continue
-    if calc not in [1, 2, 3, 4, 0]:
-        print("Número inserido inválido")
         break
-    num1 = float(input("Digite um número: "))
-    if not num1.is_integer():
-        print("Valor inválido. Digite um número inteiro.")
-        continue
-    num2 = float(input("Digite outro número: "))
-    if not num2.is_integer():
-        print("Valor inválido. Digite um número inteiro.")
-        continue
+
+
+    num1 = somente_num("Digite o Primeiro Numero: ")
+    num2 = somente_num("Digite o segundo Numero: ")
+
+#-----Fim_Menu-----#
+
+#-----Programa_Interno-----#
+
     if calc == 1:
-        resul = num1 + num2
+        resul = calc_soma(num1,num2)
         calc = "+"
     elif calc == 2:
-        resul = num1 - num2
+        resul = calc_sub(num1,num2)
         calc = "-"
     elif calc == 3:
-        resul = num1 * num2
+        resul = calc_multi(num1,num2)
         calc = "*"
     elif calc == 4:
         if num2 == 0:
             print("Não é possível dividir por zero.")
             continue
-        resul = num1 / num2
+        resul = calc_div(num1,num2)
         calc = "/"
     else:
         print("Opção inválida, digite novamente.")
         continue
+
+    #-----Fim_Programa_Interno-----#
+
+    #-----Resultado-----#
     print("-" * 80)
     print(f"A operação foi: {num1} {calc} {num2} = {resul}")
-
-# Add a try-except block to catch ValueError exceptions
-while True:
-    try:
-        num = int(input("Enter a number: "))
-        break
-    except ValueError:
-        print("Invalid input. Please enter a number.")
+    print("-" * 80)
